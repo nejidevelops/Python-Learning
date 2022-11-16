04a926f737c579ba5cb001844179bab0e337a34e
Newton Ombese <newtonombese1@gmail.com>
2022-11-16 09:36:39 +0300

    it is going on well

--- main.py
+++ main.py
@@ -3,3 +3,16 @@
 name = "Newton"
 
 print (name)
+
+# Write a program for a hospital 
+
+# Check in a patient named John Smith
+# He is 20 years old
+# He is a new Patient
+# Declare variables to store this values
+
+name = "John Smith"
+age = 20
+
+print (name)
+print (age)
