fe5c81906f3bfeb0dc1b55e91f685509a3c66ebc
Newton Ombese <newtonombese1@gmail.com>
2022-11-23 07:49:25 +0300

    working on input and int to work on addition

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 name = "Newton"
 
-print (name)
+# print (name)
 
 # Write a program for a hospital 
 
@@ -14,25 +14,45 @@ print (name)
 name = "John Smith"
 age = 20
 
-print (name)
-print (age)
+# print (name)
+# print (age)
 
 
 # List variables
 better_days = ["Berlin", "Madrid", "Washington"]
 better_days[1] = "Oslo"
-print (better_days)
+# print (better_days)
 
 
 # Print a tuple of variables
 
 tuple_of_days = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
-print (tuple_of_days[2])
+# print (tuple_of_days[2])
 
-print(tuple_of_days.count("Monday"))
+# print(tuple_of_days.count("Monday"))
 
 # Set
 
 s = {23,14,13,26,7}
 # print(s.clear())
-print (s)
+# print (s)
+
+# Arithmetic Operations
+
+num1 = 23
+num2 = 14
+# print(num1 * num2)
+# print(num1 / num2)
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 % num2)
+# print(num1 ** num2)
+
+# USer inputs
+
+x = input("Enter the value of x" )
+a = int(x)
+y = input("Enter the value of y")
+b = int(y)
+z = a + b
+print(z)
